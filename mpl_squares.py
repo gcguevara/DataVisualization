119c823c28be7858526fa120d01cada2b8beb27e
gcguevara <garrettcguevara@gmail.com>
2017-02-10 14:54:02 -0800

    Initial commit

---
+++ mpl_squares.py
@@ -0,0 +1,15 @@
+"""
+Author: Garrett Guevara
+Written/Tested: Python 3.5.2
+Purpose: Learning Data Visualization with Python Crash Course by Eric Matthes
+"""
+
+
+import matplotlib.pyplot as plt
+
+# create a basic data set
+squares = [1, 4, 9, 16, 25]
+# plot the set
+plt.plot(squares)
+# show it visually
+plt.show()
